20003225de79ed1c9de0c8d009e7ba5e47ef462e
anuj paul <paulanuj27@gmail.com>
2026-01-24 04:55:22 +0600

    js variable

---
+++ M-13 Hello JavaScript/variable.js
@@ -0,0 +1,16 @@
+// Number or Numeric  
+var age = 10;
+console.log(age);
+
+// String
+var name = 'Krishna'
+console.log(name);
+
+// Boolean 
+{let passed = false;
+
+    console.log(passed);
+}
+
+passed = true;
+console.log(passed);
