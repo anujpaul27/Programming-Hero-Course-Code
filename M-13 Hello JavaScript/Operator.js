533a186817c61d1bbeb86779f96e02e86de684f4
anuj paul <paulanuj27@gmail.com>
2026-01-24 15:11:52 +0600

    Operator

---
+++ M-13 Hello JavaScript/Operator.js
@@ -0,0 +1,18 @@
+// Assign the value 5 to x
+let x = 5;
+console.log(x);
+
+const an = 10;
+const am = 20;
+const adding = an + am;
+const difference = am - an;
+const multiple = an * am;
+const devide = am / an; // This a back word 
+const pow = 3**2  // output should be 9 
+
+console.log('Power ', pow);
+
+{let an = 100 ;
+    an = 200;
+    console.log(an );
+}
