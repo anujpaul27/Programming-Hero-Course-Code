594858b2c3a3985491c59537c25a78d48c14d69f
anuj paul <paulanuj27@gmail.com>
2026-02-27 06:40:20 +0600

    Deference var,let,const

---
+++ M-26 ES6/dataType.js
@@ -0,0 +1,22 @@
+//  var details
+// console.log(val);   // and hoisting is undefine
+var val = 20;
+val = 30;       // var can be reassign 
+var val = 40;   // var can be redeclear
+// console.log(val);
+
+
+// let details
+// console.log(value); // let reassign not applicable 
+let value = 10;
+value  = 20;    // let can be reassign
+let value2;
+// console.log(value2);
+
+
+// Const details
+const num = 10;
+// num=20;
+console.log(num);
+
+
