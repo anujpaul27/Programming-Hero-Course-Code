5bc1542a89ab29440b7283917aff783898c39bf0
anuj paul <paulanuj27@gmail.com>
2026-04-10 12:37:20 +0600

    Error and Not-Found page

---
+++ m-41-next-rending/app/not-found.js
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col mt-10 text-5xl items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <p className="text-5xl">404 page</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
