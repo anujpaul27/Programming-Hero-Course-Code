429892d1a39d0316e4bdc816de0fd16cc66ab28c
anuj paul <paulanuj27@gmail.com>
2026-04-23 06:04:17 +0600

    Use Params in next.js

--- m-41-next-rending/app/product/[id]/page.js
+++ m-41-next-rending/app/product/[id]/page.js
@@ -7,16 +7,12 @@ const ProductDetailsPage = async ({ params }) => {
   // featch the specific data from the api
   const res = await fetch(`https://dummyjson.com/products/${id}`);
   const data = await res.json();
-//   console.log(data);
+  //   console.log(data);
   return (
     <div>
       <h1 className="text-2xl ">{data.title}</h1>
       <p>{data.description}</p>
-      <Image 
-      src={data.thumbnail}
-      width={300}
-      height={300}
-       alt="image"></Image>
+      <Image src={data.thumbnail} width={300} height={300} alt="image"></Image>
     </div>
   );
 };
