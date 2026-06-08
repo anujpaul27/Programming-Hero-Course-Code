65bf2841fe0567eb93fc3ef02c95be2d805194f5
anuj paul <paulanuj27@gmail.com>
2026-06-08 21:16:07 +0600

    Create DB file for database connect

--- m-56 Full Stack Project/backend/server.js
+++ m-56 Full Stack Project/backend/server.js
@@ -1,5 +1,9 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
+const ConnectDB = require('./src/config/db');
+
+// Connect to the database
+ConnectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
