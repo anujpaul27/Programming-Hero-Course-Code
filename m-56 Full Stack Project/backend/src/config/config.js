5b7a5b557c8e4ab5397a2571674091cf3562db9c
anuj paul <paulanuj27@gmail.com>
2026-06-08 21:18:22 +0600

    Create config file for the share valid env variable with is component

---
+++ m-56 Full Stack Project/backend/src/config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+
+if (!process.env.MONGODB_URI)
+{
+    throw new Error('MONGODB_URI is not defined in environment variables');
+}
+
+
+const config = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    PORT: process.env.PORT || 3000,
+};
+
+module.exports = config;
